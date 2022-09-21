8fa031e2669d6049d1bb8dd8cd43a342cec78bd7
Tyson G Fritz <tysongfritz@gmail.com>
2022-09-20 20:41:43 -0700

    Added another example to profile-object.js

--- problem-solving/profile-object.js
+++ problem-solving/profile-object.js
@@ -50,3 +50,27 @@ function same1(arr1, arr2) {
    }
    return true;
 }
+
+//given 2 strings.
+//calculate if all characters occur the same frequency
+function sameFrequency(a, b) {
+   let aString = "" + a;
+   let bString = "" + b;
+   if (aString.length != bString.length) return false;
+
+   let aObj = {};
+   let bObj = {};
+
+   for (let char of aString) {
+      aObj[char] = aObj[char] ? aObj[char]++ : 1;
+   }
+   for (let char of bString) {
+      bObj[char] = bObj[char] ? bObj[char]++ : 1;
+   }
+
+   for (let key in aObj) {
+      if (aObj[key] !== bObj[key]) return false;
+   }
+
+   return true;
+}
