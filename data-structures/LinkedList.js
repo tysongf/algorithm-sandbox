445224657f0381adaa8b60434e17371f96fe537d
Tyson G Fritz <tysongfritz@gmail.com>
2022-09-23 03:05:10 -0700

    Added unshift function to linked list

--- data-structures/LinkedList.js
+++ data-structures/LinkedList.js
@@ -53,6 +53,19 @@ class LinkedList {
       return shiftedNode;
    }
 
+   unshift(val) {
+      let newNode = new Node(val);
+      if (!this.head) {
+         this.head = newNode;
+         this.tail = this.head;
+      } else {
+         newNode.next = this.head;
+         this.head = newNode;
+      }
+      this.length++;
+      return this;
+   }
+
    traverse() {
       let current = this.head;
       while (current) {
